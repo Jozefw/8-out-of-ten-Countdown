8537f4627d0926bc9d3f67d119b462c8f486c4a9
Jozef <Jozefw@sbcglobal.net>
2024-07-17 12:05:10 -0700

    add ref for timer

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,19 +1,20 @@
-import {useState} from 'react';
+import {useState, useRef} from 'react';
 
 export default function Timer({title,targetTime}){
 const [timerExp,setTimerExp] = useState(false)
 const [timerStart,setTimerStart] = useState(false)
 
+const timer = useRef();
 
 function HandleStart(){
-    setTimeout(()=>{
+    timer.current = setTimeout(()=>{
         setTimerExp(true);
     },targetTime * 1000)
     setTimerStart(true);
 }
 
 function HandleStop(){
-    
+    clearTimeout(timer.current);
 }
 
     return(
@@ -24,7 +25,7 @@ function HandleStop(){
             {targetTime} second{targetTime > 1 ? 's':''}
             </p>
             <p>
-                <button onClick={HandleStart} className="">
+                <button onClick={timerStart ? HandleStop : HandleStart} className="">
                     {timerStart ? 'Stop' : 'Start'} Challenge
                 </button>
                 <p className={timerStart ? 'active': undefined}>
